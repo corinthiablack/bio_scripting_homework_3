786961a62d5a73765b838ee170f6534b3e6e1a11
Corinthia R. Black <coriblack@auburn.edu>
2019-02-11 15:42:16 -0600

    debugging code
    fixed issues and cleared up output format. will now produce output file with date name in it.

--- part_2/sys_info_BLACK.sh
+++ part_2/sys_info_BLACK.sh
@@ -5,9 +5,8 @@ date=`date | awk '{print $3 $2 $6}'`
 users=`who | awk '{print $1}'`
 uptime=`uptime`
 
-echo $time
-echo $date
-echo $users
-echo $uptime
+echo "Today is $date at $time." > $date.output.txt
+echo "These users are logged into the ASC: $users" >> $date.output.txt
+echo "The uptime is currently $uptime." >> $date.output.txt
 
 
