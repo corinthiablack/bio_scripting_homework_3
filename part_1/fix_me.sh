8605d719af58ef43c419612f09ffaa1b7197d6f7
Corinthia R. Black <coriblack@auburn.edu>
2019-02-11 15:17:32 -0600

    debugging code
    removed unwanted spaces in lines

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -4,34 +4,26 @@
 read -p "Enter a:" a
 read -p "Enter b:" b
 
-          add
-    = $((a + b))
+    add = $((a + b))
         echo Addition of a and b are $add
 
-            sub
-    = $((a - b))
+    sub = $((a - b))
         echo Subtraction of a and b are $sub
 
-            mul
-    = $((a * b))
+    mul = $((a * b))
         echo Multiplication of a and b are $mul
 
-            div
-    = $((a / d))
+    div = $((a / d))
         echo division of a and b are $div
 
-            mod
-    = $((a % b))
-          echo Modulus of a
-      and b are $mod
+    mod = $((a % b))
+          echo Modulus of and b are $mod
 
       ((++a))
-          echo Increment
-          operator when applied on "a" results into a = $a
+          echo Increment operator when applied on "a" results into a = $a
 
       ((--b))
-          echo Decrement
-          operator when applied on "b" results into b = $b
+          echo Decrement operator when applied on "b" results into b = $b
 
 (
 (--c)
