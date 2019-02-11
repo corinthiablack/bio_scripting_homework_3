fdee0ecd20ce05cb3ae0bfa19c2f7a5827a726bc
Corinthia R. Black <coriblack@auburn.edu>
2019-02-11 15:46:12 -0600

    debugging FINAL TIME
    fixed minor formatting things to make output look better

--- part_2/sys_info_BLACK.sh
+++ part_2/sys_info_BLACK.sh
@@ -5,8 +5,9 @@ date=`date | awk '{print $3 $2 $6}'`
 users=`who | awk '{print $1}'`
 uptime=`uptime`
 
-echo "Today is $date at $time." > $date.output.txt
-echo "These users are logged into the ASC: $users" >> $date.output.txt
-echo "The uptime is currently $uptime." >> $date.output.txt
+echo "Today is $date at $time." > output.$date.$time.txt
+echo "These users are logged into the ASC:" >> output.$date.$time.txt
+echo $users >> output.$date.$time.txt
+echo "The uptime is currently $uptime." >> output.$date.$time.txt
 
 
